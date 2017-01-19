a29237770acf1e9c8670f5f49063add795354029
antonio <a.caristia@gmail.com>
2017-01-19 21:16:12 +0100

    aggiunto dizionari/modifica_sul_posto.py

---
+++ dizionari/modifica_sul_posto.py
@@ -0,0 +1,18 @@
+D = {"eggs": 3, "ham": 1, "spam": 2}
+
+print D
+
+#modifica di un elemento
+D["ham"] = ["grill", "bake", "fry"]
+
+print D
+
+#eliminazione di un elemento
+del D["eggs"]
+
+print D
+
+#aggiunta di un nuovo elemento
+D["brunch"] = "Bacon"
+
+print D
