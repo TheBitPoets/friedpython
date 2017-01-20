082a08e12a40bbf54a4b8aa0696c27858f84cd6c
antonio <a.caristia@gmail.com>
2017-01-20 19:56:44 +0100

    aggiunto dizionari/dizionari_matrici_sparse.py

---
+++ dizionari/dizionari_matrici_sparse.py
@@ -0,0 +1,12 @@
+# matrice trimensionale sparsa
+
+#le chiavi sono tuplr che memorizzano 
+#le posizioni delle operazioni non vuote
+
+Matrix = {}
+
+Matrix[(1,4,6)] = "ciao"
+
+Matrix[(1,5,8)] = "miao"
+
+
