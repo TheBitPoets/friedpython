c0b7f957e64b23b34a3becc88add7971f4952378
antonio <a.caristia@gmail.com>
2017-01-21 11:09:30 +0100

    aggiunto liste/indirizzamento_sezionamento_e_matrici.py

---
+++ liste/indirizzamento_sezionamento_e_matrici.py
@@ -0,0 +1,31 @@
+#   indizzamento e sezionamento non eliminano, restituiscono sempre
+#   una nuova lista
+
+#1. indirizzamento
+
+L = [0,1,2,3,4]
+
+L[2] #seleziona il terzo elemento (2), gli indici partono da zero
+
+L[-2] #seleziona il secondo elemento da destra (3)
+
+#2. sezionamento
+#
+#   il sezionamento estrae sottoparti
+
+L = ["a", "b", "c", "d"]
+
+L[1:3] #seleziono b e c partenza:fine(escluso)
+
+
+#3. matrici
+#
+#   uno dei metodi pre rappresentare matrici e' l'uso
+#   di liste annidate
+
+matrix = [[1,2,3],
+          [4,5,6],
+          [7,8,9]]
+
+matrix[2][1] #seleziono 7
+
