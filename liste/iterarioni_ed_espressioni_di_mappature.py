0024396c92f024adc955b2c03057edcdb34eb727
antonio <a.caristia@gmail.com>
2017-01-21 10:49:47 +0100

    aggiunto liste/iterazioni_ed_espressioni_di_mappature.py

---
+++ liste/iterarioni_ed_espressioni_di_mappature.py
@@ -0,0 +1,36 @@
+#1. appartenza
+
+3 in [1,2,3,4] #True|False
+
+#2. iterazione
+
+for x in [1,2,3,4]:
+    print(x, end=' ')
+
+#3. mapputura di lista
+#
+#   mapputura di lista: meccanimo per crare nuove liste
+#   applicando una espressione ad ogni elemento di una
+#   sequenza: lita, stringa, tupla
+
+res = [c*4 for x in "ciao"] # ["cccc", "iiii", "aaaa", "oooo"]
+
+#lo stesso risultato con ciclo for
+
+res = []
+for c in "ciao":
+    res.append(c*4)
+
+
+#4. funzione map()
+#
+#   la funzione map() applica una funzione a ogni elemento
+#   di una sequenza restituendo una nuova lista
+
+res = list(map(abs, [-1,-1-3]))
+
+#con ciclo for
+
+res = []
+for x in [-1,-2-3]:
+    res.append(asb(x)) 
