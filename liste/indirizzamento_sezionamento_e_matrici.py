5bc92dcea765125ca7f3e17861efe0574c640bc3
antonio <a.caristia@gmail.com>
2017-01-22 20:35:09 +0000

    Update indirizzamento_sezionamento_e_matrici.py

--- liste/indirizzamento_sezionamento_e_matrici.py
+++ liste/indirizzamento_sezionamento_e_matrici.py
@@ -6,9 +6,9 @@
 
 L = [0,1,2,3,4]
 
-L[2] #seleziona il terzo elemento (2), gli indici partono da zero
+e = L[2] #seleziono il terzo elemento (2), gli indici partono da zero
 
-L[-2] #seleziona il secondo elemento da destra (3)
+e = L[-2] #seleziono il secondo elemento da destra (3)
 
 #2. Sezionamento
 #
@@ -16,7 +16,7 @@ L[-2] #seleziona il secondo elemento da destra (3)
 
 L = ["a", "b", "c", "d"]
 
-L[1:3] #seleziono b e c partenza:fine(escluso)
+LL = L[1:3] #seleziono b e c partenza:fine(escluso)
 
 
 #3. Matrici a due dimensioni
@@ -28,5 +28,5 @@ matrix = [[1,2,3],
           [4,5,6],
           [7,8,9]]
 
-matrix[2][1] #seleziono 7
+e = matrix[2][1] #seleziono 7
 
