3320a0b7d8b0a02aa90264ba066cd9f073552875
antonio <a.caristia@gmail.com>
2017-01-22 19:59:18 +0000

    Update operazioni_di_base.py

--- liste/operazioni_di_base.py
+++ liste/operazioni_di_base.py
@@ -1,22 +1,25 @@
+#1. Creare una lista
+
 L = [1,2,3,4,5]
 
-#1. len()
+#2. Lunghezza della lista: len()
 
 n_elem = len(L)
 
-#2. concatenazione
+#3. Concatenazione
 
-#L'operatore + effettua la concatenazione tra lista
-#si aspetta lo stesso tipo di sequenza su ambo i lati
-#altrimenti viene generato un errore
+# L'operatore + effettua la concatenazione tra liste
 
 L = L + [6,7] #ok
+
+# + si aspetta lo stesso tipo di sequenza su ambo i lati
+# altrimenti viene generato un errore
 
 L = L + "6" + "7" #errore, L e' sequenza lista, "6" e "7" sequenza stringa
 
 L = L + list("6") #oppure L = str(L) + "6"
 
-#3. Ripetizione
+#4. Ripetizione
 
 L = ["ciao"]
 
